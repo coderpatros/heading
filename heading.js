64a276c9958b910e32839d926ea1aa64828cf45a
Patrick Dwyer <coderpatros@outlook.com>
2021-03-11 12:49:46 +1000

    Update heading.js

--- heading.js
+++ heading.js
@@ -1,12 +1,19 @@
 var headingElement = document.getElementById("heading");
 
-window.addEventListener("deviceorientation", function(event) {
-    if (event.webkitCompassHeading !== undefined)
-    {
-        headingElement.innerText = event.webkitCompassHeading;
-    }
-    else
-    {
-        headingElement.innerText = event.alpha;
-    }
-}, true);
+if (window.DeviceOrientationEvent) {
+    window.addEventListener("deviceorientation", function(event) {
+        if (event.webkitCompassHeading !== undefined)
+        {
+            headingElement.innerText = event.webkitCompassHeading;
+        }
+        else
+        {
+            headingElement.innerText = event.alpha;
+        }
+
+        handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
+    }, true);
+}
+else {
+    headingElement.innerText = "No DeviceOrientationEvent";
+}
